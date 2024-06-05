eae2f2916d969cc63ef6138dca836d6877a67784
nabilland <nabillasolo2015@gmail.com>
2024-06-05 21:07:21 +0700

    create blade templates

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Best Admin Panels" />
+    <meta name="keywords" content="Admin, Dashboard, Bootstrap4, Sass, CSS3, HTML5, Responsive Dashboard, Responsive Admin Template, Admin Template, Best Admin Template, Bootstrap Template, Themeforest" />
+    <meta name="author" content="Bootstrap Gallery" />
+    <link rel="canonical" href="https://www.bootstrap.gallery/">
+    <meta property="og:url" content="https://www.bootstrap.gallery">
+    <meta property="og:title" content="Admin Templates - Dashboard Templates | Bootstrap Gallery">
+    <meta property="og:description" content="Marketplace for Bootstrap Admin Dashboards">
+    <meta property="og:type" content="Website">
+    <meta property="og:site_name" content="Bootstrap Gallery">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.svg') }}" />
+    <title>Best Admin Themes</title>
+
+    <!-- Common CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('fonts/icomoon/icomoon.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/main.min.css') }}" />
+
+    <!-- Other CSS includes plugins - Cleanedup unnecessary CSS -->
+    <!-- Chartist css -->
+    <link href="{{ asset('vendor/chartist/css/chartist.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/chartist/css/chartist-custom.css') }}" rel="stylesheet" />
+</head>
---
+++ resources/views/layouts/page-loading.blade.php
@@ -0,0 +1,11 @@
+<!-- Loading starts -->
+<div class="loading-wrapper">
+    <div class="loading">
+        <div class="img"></div>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</div>
