4c98b423e6515334841d5c03c57b43a06da03628
naurahhikmah <naurahhikmahaulia@gmail.com>
2024-06-07 04:10:10 +0700

    manajemen user

--- resources/views/dashboard/UserManagement.php
+++ resources/views/dashboard/UserManagement.php
@@ -1,6 +1,66 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Best Admin Panels" />
+    <meta name="keywords" content="Admin, Dashboard, Bootstrap4, Sass, CSS3, HTML5, Responsive Dashboard, Responsive Admin Template, Admin Template, Best Admin Template, Bootstrap Template, Themeforest" />
+    <meta name="author" content="Bootstrap Gallery" />
+    <link rel="canonical" href="https://www.bootstrap.gallery/">
+    <meta property="og:url" content="https://www.bootstrap.gallery">
+    <meta property="og:title" content="Admin Templates - Dashboard Templates | Bootstrap Gallery">
+    <meta property="og:description" content="Marketplace for Bootstrap Admin Dashboards">
+    <meta property="og:type" content="Website">
+    <meta property="og:site_name" content="Bootstrap Gallery">
+    <link rel="shortcut icon" href="{{ asset('img/logo-pmi-full.png') }}" />
+    <title>Sistem Penanggulangan Bencana PMI Solo</title>
+
+    <!-- Common CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('fonts/icomoon/icomoon.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/main.min.css') }}" />
+
+    <!-- Other CSS includes plugins - Cleanedup unnecessary CSS -->
+    <!-- Chartist css -->
+    <link href="{{ asset('vendor/chartist/css/chartist.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/chartist/css/chartist-custom.css') }}" rel="stylesheet" />
+</head>
+
+@include('layouts.head')
+
+<body>
+    @include('layouts.page-loading')
+
+    <div class="app-wrap">
+        @include('layouts.header')
+        <div class="app-container">
+            @include('layouts.sidebar')
+            <div class="app-main">
+                <main>
+                    <!-- BEGIN .main-heading -->
+                    <header class="main-heading">
+                        @yield('heading')
+                    </header>
+                    <!-- END: .main-heading -->
+                    <!-- BEGIN .main-content -->
+                    <div class="main-content">
+                        @yield('content')
+                    </div>
+                    <!-- END: .main-content -->
+                </main>
+            </div>
+        </div>
+    </div>
+
+    @include('layouts.footer')
+    @include('layouts.scripts')
+</body>
+
+</html>
     <style>
         /* Reset CSS */
         * {
