76342fa6201890fb0a6f38a87188f7fe1c5f6701
yuzzarr <yuzzarrizkym@gmail.com>
2024-06-09 14:54:57 +0700

    Perubahan Database

--- database/migrations/2024_06_05_135622_create_dampak_table.php
+++ database/migrations/2024_06_05_135622_create_dampak_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('dampak', function (Blueprint $table) {
             $table->id('id_dampak');
-            $table->foreignId('id_korban_terdampak')->references('id_korban_terdampak')->on('korban_terdampak');
-            $table->foreignId('id_korban_jiwa')->references('id_korban_jiwa')->on('korban_jiwa');
-            $table->foreignId('id_kerusakan_rumah')->references('id_kerusakan_rumah')->on('kerusakan_rumah');
-            $table->foreignId('id_kerusakan_fasilitas')->references('id_kerusakan_fasilitas')->on('kerusakan_fasilitas');
-            $table->foreignId('id_kerusakan_infrastruktur')->references('id_kerusakan_infrastruktur')->on('kerusakan_infrastruktur');
-            $table->foreignId('id_lokasi_shelter')->references('id_lokasi_shelter')->on('shelter');
+            $table->foreignId('id_korban_terdampak')->references('id_korban_terdampak')->on('korban_terdampak')->onDelete('cascade');
+            $table->foreignId('id_korban_jiwa')->references('id_korban_jiwa')->on('korban_jiwa')->onDelete('cascade');
+            $table->foreignId('id_kerusakan_rumah')->references('id_kerusakan_rumah')->on('kerusakan_rumah')->onDelete('cascade');
+            $table->foreignId('id_kerusakan_fasilitas')->references('id_kerusakan_fasilitas')->on('kerusakan_fasilitas')->onDelete('cascade');
+            $table->foreignId('id_kerusakan_infrastruktur')->references('id_kerusakan_infrastruktur')->on('kerusakan_infrastruktur')->onDelete('cascade');
+            $table->foreignId('id_lokasi_shelter')->references('id_lokasi_shelter')->on('shelter')->onDelete('cascade');
             $table->timestamps();
         });
     }
