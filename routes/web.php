c3d43e4da639372715babddc242a276aeb3d6e60
nabilland <nabillasolo2015@gmail.com>
2024-06-11 16:20:07 +0700

    add user management view (naurah) & create new folders for each modules

--- routes/web.php
+++ routes/web.php
@@ -2,37 +2,46 @@
 
 use Illuminate\Support\Facades\Route;
 
+// ROUTES UNTUK DASHBOARD
 Route::get('/', function () {
     return view('dashboard.dashboard');
 });
 
-Route::get('/form', function () {
-    return view('forms.form');
+// ROUTES UNTUK USER MANAGEMENT
+Route::get('/user-management', function(){
+    return view('user.user-management');
 });
 
-Route::get('/laporan-situasi', function () {
-    return view('dashboard.laporan-situasi');
-});
-
-Route::get('/tambah-lapsit', function () {
-    return view('forms.tambah-lapsit');
-});
+// ROUTES UNTUK KEJADIAN
 
 
-// executive summary
-Route::get('/executive-summary', function () {
-    return view('dashboard.exsum');
-});
 
-
+// ROUTES UNTUK ASSESSMENT
 Route::get('/form-assessment', function () {
     return view('form-assessment');
 });
 
+// ROUTES UNTUK LAPORAN SITUASI
+Route::get('/laporan-situasi', function () {
+    return view('lapsit.laporan-situasi');
+});
+
+Route::get('/tambah-lapsit', function () {
+    return view('lapsit.tambah-lapsit');
+});
+
+// ROUTES UNTUK EXECUTIVE SUMMARY
+Route::get('/executive-summary', function () {
+    return view('executive-summary.exsum');
+});
+
+
+// ROUTES UNTUK FLASH REPORT
 Route::get('/flash-report', function(){
     return view('flash-report.flash-report');
 });
 
-Route::get('/user-management', function(){
-    return view('dashboard.UserManagement');
+// CONTOH
+Route::get('/form', function () {
+    return view('forms.form');
 });
