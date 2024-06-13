fe919f4e160471a19081b7d99d1dd5f679267c77
nabilland <nabillasolo2015@gmail.com>
2024-06-13 10:58:50 +0700

    add UserController

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,44 @@
-@extends('layouts.head')
+<!doctype html>
+<html lang="en">
 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="Best Admin Panels" />
+    <meta name="keywords" content="Admin, Dashboard, Bootstrap4, Sass, CSS3, HTML5, Responsive Dashboard, Responsive Admin Template, Admin Template, Best Admin Template, Bootstrap Template, Themeforest" />
+    <meta name="author" content="Bootstrap Gallery" />
+    <link rel="canonical" href="https://www.bootstrap.gallery/">
+    <meta property="og:url" content="https://www.bootstrap.gallery">
+    <meta property="og:title" content="Admin Templates - Dashboard Templates | Bootstrap Gallery">
+    <meta property="og:description" content="Marketplace for Bootstrap Admin Dashboards">
+    <meta property="og:type" content="Website">
+    <meta property="og:site_name" content="Bootstrap Gallery">
+    <link rel="shortcut icon" href="{{ asset('img/logo-pmi-full.png') }}" />
+    <title>Sistem Penanggulangan Bencana PMI Solo</title>
+
+    <!-- Common CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('fonts/icomoon/icomoon.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+    <!-- <link rel="stylesheet" href="{{ asset('css/main.min.css') }}" /> -->
+
+
+    <!-- Other CSS includes plugins - Cleanedup unnecessary CSS -->
+    <!-- Chartist css -->
+    <link href="{{ asset('vendor/chartist/css/chartist.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/chartist/css/chartist-custom.css') }}" rel="stylesheet" />
+
+    <!-- Data Tables -->
+    <link rel="stylesheet" href="{{ asset('vendor/datatables/dataTables.bs4.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/datatables/dataTables.bs4-custom.css') }}" />
+
+    <!-- input lokasi dengan peta -->
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+</head>
 
 <body class="login-bg">
 
