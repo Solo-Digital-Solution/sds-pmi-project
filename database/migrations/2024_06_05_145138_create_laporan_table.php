d2accbf6d1c2ad62ef6abf92c432e58ff88c782d
yuzzarr <yuzzarrizkym@gmail.com>
2024-06-24 16:22:59 +0700

    Perubahan Database

--- database/migrations/2024_06_05_145138_create_laporan_table.php
+++ database/migrations/2024_06_05_145138_create_laporan_table.php
@@ -17,14 +17,11 @@ return new class extends Migration
             $table->foreignId('id_dampak')->references('id_dampak')->on('dampak')->onDelete('cascade');
             $table->foreignId('id_mobilisasi')->references('id_mobilisasi')->on('mobilisasi')->onDelete('cascade');
             $table->foreignId('id_giat_pmi')->references('id_giat_pmi')->on('giat_pmi')->onDelete('cascade');
-            $table->foreignId('id_personil_dihubungi')->references('id_personil_dihubungi')->on('personil_dihubungi')->onDelete('cascade');
-            $table->foreignId('id_petugas_posko')->references('id_petugas_posko')->on('petugas_posko')->onDelete('cascade');
-            $table->foreignId('id_dokumentasi')->references('id_dokumentasi')->on('dokumentasi')->onDelete('cascade');
             $table->string('giat_pemerintah');
             $table->string('kebutuhan');
             $table->string('hambatan');
             $table->string('nama_laporan');
-            $table->dateTime('update');
+            $table->dateTime('update')->nullable();
             $table->timestamps();
         });
     }
